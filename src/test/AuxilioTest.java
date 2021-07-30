b77c7f5452987391085131a9124801a38b498aa4
Gabs343 <gabriellopezp343@gmail.com>
2021-07-29 22:39:27 -0300

    casos de prueba

--- src/test/AuxilioTest.java
+++ src/test/AuxilioTest.java
@@ -18,6 +18,7 @@ public class AuxilioTest {
 		
 		auxilioServ.agregarTipoDeAuxilio("Bateria", TipoReparacion.SIMPLE, false, new BigDecimal(100));
 		auxilioServ.agregarTipoDeAuxilio("Choque", TipoReparacion.SIMPLE, false, new BigDecimal(500));
+		auxilioServ.agregarTipoDeAuxilio("Pinchadura", TipoReparacion.SIMPLE, true, new BigDecimal(700));
 		auxilioServ.agregarTipoDeAuxilio("Daño Motor", TipoReparacion.COMPLEJO, true, new BigDecimal(1000));
 		
 		try {
@@ -27,18 +28,24 @@ public class AuxilioTest {
 			System.out.println("-------------------------------------");
 			
 			System.out.println("CASO 2: Cliente con cobertura clasica sin deuda y con un vehiculo menor a 3 tons se le asigna la mini-grua");
-			Pedido pedido_2 = auxilioServ.crearPedido("Gabriel", "Lopez", new BigDecimal(5000), "AABBCC", 2050.4, "Calle-1", TipoCobertura.CLASSIC, auxilioServ.getTipoAuxilioPorDescripcion("Daño Motor"));
+			Pedido pedido_2 = auxilioServ.crearPedido("Gabriel", "Lopez", new BigDecimal(5000), "AABBCC", 2050.4, "Calle-2", TipoCobertura.CLASSIC, auxilioServ.getTipoAuxilioPorDescripcion("Daño Motor"));
 			auxilioServ.agregarPedido(pedido_2);
 			System.out.println("-------------------------------------");
 			
-			System.out.println("CASO 3: Cliente con cobertura clasica realizando dos pedidos, en el primero queda debiendo 200, al pedir el segundo paga los 200 anteriores pero no le alcanza para cubrir el segundo pedido");
-			Pedido pedido_3 = auxilioServ.crearPedido("Alejandro", "Perez", new BigDecimal(1000), "DDEEFF", 2500.0, "Calle-3", TipoCobertura.CLASSIC, auxilioServ.getTipoAuxilioPorDescripcion("Choque"));
+			System.out.println("CASO 3: Cliente con cobertura clasica sin deuda y con un vehiculo mayor a 3 tons se le asigna la gran-grua");
+			Pedido pedido_3 = auxilioServ.crearPedido("Carlos", "Martinez", new BigDecimal(10000), "XXYYZZ", 3500.0, "Calle-3", TipoCobertura.CLASSIC, auxilioServ.getTipoAuxilioPorDescripcion("Daño Motor"));
 			auxilioServ.agregarPedido(pedido_3);
-			
-			Pedido pedido_4 = auxilioServ.crearPedido("Alejandro", "Perez", new BigDecimal(1000), "DDEEFF", 2500.0, "Calle-3", TipoCobertura.CLASSIC, auxilioServ.getTipoAuxilioPorDescripcion("Choque"));
-			auxilioServ.agregarPedido(pedido_4);
 			System.out.println("-------------------------------------");
-	
+			
+			System.out.println("CASO 4: Cliente con cobertura clasica realizando dos pedidos, en el primero queda debiendo 200, al pedir el segundo paga los 200 anteriores pero no le alcanza para cubrir el segundo pedido");
+			Pedido pedido_4 = auxilioServ.crearPedido("Alejandro", "Perez", new BigDecimal(1000), "DDEEFF", 2500.0, "Calle-4", TipoCobertura.CLASSIC, auxilioServ.getTipoAuxilioPorDescripcion("Choque"));
+			auxilioServ.agregarPedido(pedido_4);
+			
+			Pedido pedido_5 = auxilioServ.crearPedido("Alejandro", "Perez", new BigDecimal(1000), "DDEEFF", 2500.0, "Calle-5", TipoCobertura.CLASSIC, auxilioServ.getTipoAuxilioPorDescripcion("Choque"));
+			auxilioServ.agregarPedido(pedido_5);
+			System.out.println("-------------------------------------");
+			
+			
 			
 		}catch(TipoAuxilioNotFound e) {
 			e.getMessage();
